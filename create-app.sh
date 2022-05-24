8306a07e9e01072c5645022b1c6aaca4343cfce8
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-05-24 22:26:12 +0530

    Update create-app.sh

--- create-app.sh
+++ create-app.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Author:Prayag Sangode
-# Install Maven on CentOS7
-yum -y install maven
 # Create a Spring Boot Application
-mkdir java-mavn-app
-cd java-mavn-app
+mkdir java-mvn-app
+cd java-mvn-app
 # Check java version. Below we are downloading as per java version 8
 rpm -qa | grep java
 curl https://start.spring.io/starter.tgz -d dependencies=webflux,actuator -d javaVersion=8 | tar -xzvf -
