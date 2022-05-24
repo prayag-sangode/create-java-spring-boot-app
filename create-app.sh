8e189891292403dc0383a78d04ce317662717496
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-05-24 22:52:56 +0530

    Update create-app.sh

--- create-app.sh
+++ create-app.sh
@@ -3,8 +3,8 @@
 # Create a Spring Boot Application
 # Check java version. Below we are downloading as per java version 8
 mkdir ~/java-mvn-app
+cd ~/java-mvn-app
 rpm -qa | grep java
 curl https://start.spring.io/starter.tgz -d dependencies=webflux,actuator -d javaVersion=8 | tar -xzvf ~/java-mvn-app
-cd ~/java-mvn-app
 ./mvnw install
 ls -l ~/java-mvn-app/target/*.jar
